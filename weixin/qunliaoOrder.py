b6be91d54f19f3c79cebeed888e61ba9344b797f
Unknown <kiss2110000@163.com>
2019-01-30 02:07:41 +0800

    修改了多商品添加

--- weixin/qunliaoOrder.py
+++ weixin/qunliaoOrder.py
@@ -3,83 +3,122 @@ import openpyxl
 from wxpy import *
 
 
-bot = Bot(console_qr=True, cache_path=True)
+bot = Bot(cache_path=True)
 order = bot.groups().search(r'订单群')
 province = ['北京', '天津', '上海', '重庆', '河北', '山西', '辽宁', '吉林', '黑龙江', '江苏', '浙江', '安徽',
             '福建', '江西', '山东', '河南', '湖北', '湖南', '广东', '海南', '四川', '贵州', '云南', '陕西',
-            '甘肃', '青海', '台湾', '内蒙古自治区', '广西壮族自治区', '西藏自治区', '宁夏回族自治区',
-            '新疆维吾尔自治区', '香港特别行政区', '澳门特别行政区']
+            '甘肃', '青海', '台湾', '内蒙古', '广西壮族', '西藏', '宁夏回族', '新疆维吾尔', '香港', '澳门']
 products = [""]
+
 
 @bot.register(order, except_self=False)
 def reply_order(msg):
     cont = msg.text
+    print(cont)
     if '数量' in cont:
+        # 提示所有订单的数量
         wb = openpyxl.load_workbook(filename="order_book.xlsx")
         ws = wb.active
-        rows = ws.max_row
-        return '订单共计{}条!'.format(rows-1)
+        names = [i.value for i in tuple(ws.columns)[3] if i.value]
+        print('订单共计{}条!'.format(len(names)-1))
+        return '订单共计{}条!'.format(len(names)-1)
     if '订单' in cont:
+        # 列出所有订单的内容
         wb = openpyxl.load_workbook(filename="order_book.xlsx")
         ws = wb.active
         rows = tuple(ws.rows)[1:]
-        order = ""
-        for row in rows:
-            print(row)
-            info = [cell.value for cell in row if cell.value]
-            print(info)
-            order += ",".join(info) + '\n'
-            print(order)
-        return order
+        # print(rows)
+        content = "所有订单:\n"
+        for i, row in enumerate(rows):
+            if row[7].value:
+                style = rows[i:i+row[7].value]
+                # print(style)
+                prods = []
+                prices = []
+                numbers = []
+                for each in style:
+                    prods.append(each[0].value)
+                    prices.append(each[1].value)
+                    numbers.append(each[2].value)
+                print(prices)
+                print(numbers)
+                p_n = [str(price)+'x'+str(number) if number != 1 else str(price) for price, number in zip(prices,numbers)]
+                print(p_n)
+                cells = ["、".join(prods), "、".join(p_n), row[3].value, row[4].value, row[5].value]
+                info = ",".join(cells)
+                # print(info)
+                content += info + '\n\n'
+        print(content)
+        return content
 
-    print(cont)
-    # 将文本中的“，”替换为空格,方便查看字符边缘
-    cont = cont.replace('，', ' ')
-    # 首先找到电话
-    find = re.search(r'\d{11}', cont)
-    if find is None:
-        return "没有找到11位的手机号码!"
-    tel = find.group()
+    # 去除空格，中文逗号替换为英文逗号，再按逗号分离文本
+    cont = cont.replace(' ', '')
+    cont = cont.replace('，', ',')
+    order_info = cont.split(",")
 
-    # 将消息中的电话删除后，再过滤姓名和地址
-    cont = cont.replace(tel, '')
-    name, addr = None, None
+    # 买方、卖方、联系人、电话、产品名称、规格型号、数量、单价、总价（是否包含运费）、交付方式
+    if len(order_info) == 5:
+        order_data = []
+        # 以顿号分离产品和价格
+        prods = order_info[0].split("、")
+        prices = order_info[1].split("、")
+        if len(prods) != len(prices):
+            return "错误：\n产品名数量和价格数量不相等！"
 
-    finds = re.findall(r"\b\w+\b", cont)
-    for i in finds:
-        size = len(i)
-        if size <= 8:
-            # 如果文字数量小于8，则视为名称
-            name = i
-        else:
-            # 对大于8个的文本，看看是否包含省份
-            for each in province:
-                if each in i:
-                    addr = i
-                    break
-    if name is None:
-        return "没有找到小于8个汉字名字,!"
-    if addr is None:
-        return "没有找到包含省、市、直辖市、特别行政区的地址!"
-    # 买方、卖方、联系人、电话、产品名称、规格型号、数量、单价、总价（是否包含运费）、交付方式、
-    print("日期：{} 姓名：{} 电话：{} 地址：{}".format(msg.create_time, name, tel, addr))
+        for i in range(len(prods)):
+            prod = prods[i]
+            price = prices[i]
+            price = price.replace('X', 'x')
+            price = price.split("x")
+            if len(price) == 1 and price[0].isdigit():
+                price, number = int(price[0]), 1
+            elif len(price) == 2 and price[0].isdigit() and price[1].isdigit():
+                price, number = int(price[0]), int(price[1])
+            else:
+                return "错误：\n价格和数量格式不对！例:价格x数量(150x3)、价格(150)"
 
-    date = msg.create_time.strftime('%Y-%m-%d %H:%M:%S')
-    prod = ''
-    price = ""
-    number = ""
-    # 日期 商品 价格 数量 买家 电话 地址
-    order_data = [prod, price, number, name, tel, addr, date]
-    # print(order_data)
-    wb = openpyxl.load_workbook(filename="order_book.xlsx")
-    ws = wb.active
-    ws.insert_rows(2)
-    for clo in range(1, 8):
-        ws.cell(row=2, column=clo).value = order_data[clo-1]
-    # 文件打开时，保存失败！无报错!
-    wb.save('order_book.xlsx')
+            if i == 0:
+                prod_num = len(prods)
+                name = order_info[2]
+                tel = order_info[3]
+                if len(tel) != 11 or tel.isdigit() is False:
+                    return "错误：\n手机号码不是11位数或者不是纯数字号码！"
+                addr = None
+                for each in province:
+                    if each in order_info[4]:
+                        addr = order_info[4]
+                        break
+                if addr is None:
+                    return "错误：\n地址中没有包含省、直辖市、自治区、特别行政区的地址!"
+                date = msg.create_time.strftime('%H:%M:%S %Y-%m-%d')
 
-    return "总共添加{}条订单!".format(ws.max_row-1)
+                # 种类 商品 价格 数量 买家 电话 地址  日期
+                order_data.append([prod, price, number, name, tel, addr, date, prod_num])
+            else:
+                order_data.append([prod, price, number, "", "", "", "", ""])
+        print(order_data)
+        # 写入数据
+        wb = openpyxl.load_workbook(filename="order_book.xlsx")
+        ws = wb.active
+        for i, prod in enumerate(order_data):
+            print(123)
+            print(i)
+            ws.insert_rows(2+i)
+            print(prod)
+            for clo in range(1, 9):
+                ws.cell(row=2+i, column=clo).value = prod[clo-1]
+        # 文件打开时，保存失败！无报错!
+        wb.save('order_book.xlsx')
+
+        wb = openpyxl.load_workbook(filename="order_book.xlsx")
+        ws = wb.active
+        # 获取姓名列
+
+        names = [i.value for i in tuple(ws.columns)[3] if i.value]
+        print(names)
+        print("总共添加{}条订单!".format(len(names)-1))
+        return "总共添加{}条订单!".format(len(names)-1)
+
 
 embed()
 
