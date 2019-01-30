c417f943405658ce62c18a54b344a8835ed472f3
Unknown <kiss2110000@163.com>
2019-01-30 16:01:44 +0800

    添加按人名搜索功能

--- weixin/order/qunliaoOrder.py
+++ weixin/order/qunliaoOrder.py
@@ -31,6 +31,12 @@ order_group.send("下单工具已上线!")
 def reply_order(msg):
     cont = msg.text
     print(cont)
+
+    # 去除空格，中文逗号替换为英文逗号，再按逗号分离文本
+    cont = cont.replace(' ', '')
+    cont = cont.replace('，', ',')
+    finds = cont.split(",")
+
     if '数量' in cont:
         # 提示所有订单的数量
         wb = openpyxl.load_workbook(filename=excelFile)
@@ -38,6 +44,30 @@ def reply_order(msg):
         names = [i.value for i in tuple(ws.columns)[3] if i.value]
         print('订单共计{}条!'.format(len(names)-1))
         return '订单共计{}条!'.format(len(names)-1)
+    if '查找' in cont:
+        if len(finds) == 2 and finds[1].isalpha():
+            # 列出所有订单的内容
+            wb = openpyxl.load_workbook(filename=excelFile)
+            ws = wb.active
+            rows = tuple(ws.rows)[1:]
+            content = "查找的订单:\n"
+            # 循环每行元组，如果种类(index=7)里有数字,说明是一条订单,如果名称(index=3)相同
+            for i, row in enumerate(rows):
+                if row[7].value and row[3].value == finds[1]:
+                    style = rows[i:i + row[7].value]
+                    prods = []
+                    prices = []
+                    numbers = []
+                    for each in style:
+                        prods.append(each[0].value)
+                        prices.append(each[1].value)
+                        numbers.append(each[2].value)
+                    p_n = [str(price)+'x'+str(number) if number != 1 else str(price) for price, number in zip(prices, numbers)]
+                    cells = ["、".join(prods), "、".join(p_n), row[3].value, row[4].value, row[5].value]
+                    info = ",".join(cells)
+                    content += info + '\n\n'
+            print(content)
+            return content
     if '订单' in cont:
         # 列出所有订单的内容
         wb = openpyxl.load_workbook(filename=excelFile)
@@ -45,6 +75,7 @@ def reply_order(msg):
         rows = tuple(ws.rows)[1:]
         # print(rows)
         content = "所有订单:\n"
+        # 循环每行，如果种类里有数字,说明是一条订单
         for i, row in enumerate(rows):
             if row[7].value:
                 style = rows[i:i+row[7].value]
@@ -56,13 +87,9 @@ def reply_order(msg):
                     prods.append(each[0].value)
                     prices.append(each[1].value)
                     numbers.append(each[2].value)
-                print(prices)
-                print(numbers)
                 p_n = [str(price)+'x'+str(number) if number != 1 else str(price) for price, number in zip(prices,numbers)]
-                print(p_n)
                 cells = ["、".join(prods), "、".join(p_n), row[3].value, row[4].value, row[5].value]
                 info = ",".join(cells)
-                # print(info)
                 content += info + '\n\n'
         print(content)
         return content
@@ -70,12 +97,8 @@ def reply_order(msg):
     # 买方、卖方、联系人、电话、产品名称、规格型号、数量、单价、总价（是否包含运费）、交付方式
     # 例如：小粉刷，150x3，冯颖，18201098888，山东德州德城区
 
-    # 去除空格，中文逗号替换为英文逗号，再按逗号分离文本
-    cont = cont.replace(' ', '')
-    cont = cont.replace('，', ',')
-    order_info = cont.split(",")
-
-    if len(order_info) == 5:
+    if len(finds) == 5:
+        order_info = finds
         order_data = []
         # 以顿号分离产品和价格
         prods = order_info[0].split("、")
